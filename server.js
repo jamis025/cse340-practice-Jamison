28aa44ac8e66382395f144e7734985571cc31f0a
jamis025 <jamiso25@byui.edu>
2026-09-23 09:25:34 -0700

    Update greeting and configure dynamic port
    Updated the greeting to use the JC variable and added support for dynamic port configuration.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 const name = process.env.NAME; // <-- NEW
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
+    res.send(`Hello, ${JC}!`); // <-- UPDATED
 });
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });

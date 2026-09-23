d187b361f396f582e948e120f6018306104db848
jamis025 <jamiso25@byui.edu>
2026-09-23 09:20:53 -0700

    Add new route to server

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
+app.get('/new-route', (req, res) => {
+    res.send('This is a new route!');
+});
